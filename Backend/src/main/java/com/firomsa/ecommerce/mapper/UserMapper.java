25485a063210e4a4c76a00937fb0c3bfd516f842
firomsa <firomsassf@gmail.com>
2025-06-15 02:48:01 +0300

    fix - fixed dto mapper

--- Backend/src/main/java/com/firomsa/ecommerce/mapper/UserMapper.java
+++ Backend/src/main/java/com/firomsa/ecommerce/mapper/UserMapper.java
@@ -9,29 +9,27 @@ import com.firomsa.ecommerce.model.User;
 
 public class UserMapper {
     public static UserResponseDTO toDTO(User user){
-        UserResponseDTO userResponseDTO = new UserResponseDTO();
-        userResponseDTO.setEmail(user.getEmail());
-        userResponseDTO.setFirstName(user.getFirstName());
-        userResponseDTO.setId(user.getId().toString());
-        userResponseDTO.setLastName(user.getLastName());
-        userResponseDTO.setUserName(user.getUserName());
-        userResponseDTO.setRole(user.getRole().toString());
-        userResponseDTO.setIsActive(Boolean.toString(user.isActive()));
-        userResponseDTO.setPassword(user.getPassword());
-        return userResponseDTO;
+        return UserResponseDTO.builder()
+            .email(user.getEmail())
+            .firstName(user.getFirstName())
+            .id(user.getId().toString())
+            .lastName(user.getLastName())
+            .userName(user.getUserName())
+            .role(user.getRole().toString())
+            .isActive(Boolean.toString(user.isActive()))
+            .build();
     }
 
     public static User toModel(UserRequestDTO userRequestDTO){
-        User user = new User();
-        user.setLastName(userRequestDTO.getLastName());
-        user.setFirstName(userRequestDTO.getFirstName());
-        user.setUserName(userRequestDTO.getUserName());
-        user.setEmail(userRequestDTO.getEmail());
-        user.setPassword(userRequestDTO.getPassword());
-        user.setActive(Boolean.parseBoolean(userRequestDTO.getIsActive()));
-        user.setRole(Enum.valueOf(Role.class, userRequestDTO.getRole()));
-        user.setCreatedAt(LocalDateTime.now());
-        user.setUpdatedAt(LocalDateTime.now());
-        return user;
+        LocalDateTime now = LocalDateTime.now();
+        return User.builder()
+            .lastName(userRequestDTO.getLastName())
+            .firstName(userRequestDTO.getFirstName())
+            .userName(userRequestDTO.getUserName())
+            .email(userRequestDTO.getEmail())
+            .password(userRequestDTO.getPassword())
+            .createdAt(now)
+            .updatedAt(now)
+            .build();
     }
 }
