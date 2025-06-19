95cf9e9ea77e53ad848e50ff7aed62a002bd3c26
firomsa <firomsassf@gmail.com>
2025-06-20 00:46:06 +0300

    feat - added repository interface for entities

--- Backend/src/main/java/com/firomsa/ecommerce/repository/AddressRepository.java
+++ Backend/src/main/java/com/firomsa/ecommerce/repository/AddressRepository.java
@@ -1,9 +1,13 @@
 package com.firomsa.ecommerce.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.firomsa.ecommerce.model.Address;
+import com.firomsa.ecommerce.model.User;
+
 
 public interface AddressRepository extends JpaRepository<Address, Integer>{
-
+    Optional<Address>  findByUserAndActive(User user, Boolean active);
 }
